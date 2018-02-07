bd5f55a5aa9aa78c02b454fc9b98eb123fd3d12d
Melisa Pérez <soymelisa@yahoo.com>
2018-02-07 17:10:52 -0600

    Cambié las fotos por nuevos de la película Contacto y el Firebase quedó listo para empezar

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
 info:any = {};
 cargada:boolean =false;
+cargada_sobre_mi:boolean=false;
+equipo:any[] = [];
 
   constructor( public http:Http ) {
+    this.carga_info(),
+    this.carga_sobre_mi();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
     .subscribe( data =>{
-      console.log(data.json());
+      //console.log(data.json());
       this.cargada =true;
       this.info = data.json();
-    })
+      })
+  }
+
+  public carga_sobre_mi(){
+    this.http.get("https://melisaproyecto.firebaseio.com/equipo.json")
+    .subscribe( data =>{
+      console.log(data.json());
+      this.cargada_sobre_mi =true;
+      this.equipo = data.json();
+      })
   }
 }
