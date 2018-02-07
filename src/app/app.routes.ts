ed85bf8b249fb139e8ca9c01f4eb89a70dcde98e
Melisa Pérez <soymelisa@yahoo.com>
2018-02-07 12:22:16 -0600

    Agregamos modificiones al menú para que cambie y quite partes como de blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,10 @@ import {
 } from "./components/index.paginas";
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
       { path: 'producto', component: ProdcutoItemsComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, { useHash:true } );
