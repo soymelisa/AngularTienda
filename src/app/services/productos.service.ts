b6e6f007ec635064b12f93c2689fad656f6c855b
Melisa Pérez <soymelisa@yahoo.com>
2018-02-08 16:53:38 -0600

    firebase corregida. Agregamos desde la otra vez las imágenes y servicio de productos

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,16 +5,21 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  cargando_p:boolean =false; //que me mantenga algo así como un loading
 
   constructor( private http:Http ) {
     this.cargar_productos();
   }
 
   public cargar_productos(){
+
+    this.cargando_p = true;
+
     if (this.productos.length === 0) {
-      this.http.get('https://melisaproyecto.firebaseio.com/productos_idx')
+      this.http.get('https://melisaproyecto.firebaseio.com/productos_idx.json')
       .subscribe( res => {
         console.log( res.json() );
+        this.cargando_p = false;
       })
     }
   }
