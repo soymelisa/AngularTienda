f9e9d0de5d08d654488bd4dfafd6f1e8ec59d065
Melisa Pérez <soymelisa@yahoo.com>
2018-02-09 00:27:24 -0600

    Ya coincide cada imagen con el comentario correspondiente con el cod ruteado

--- src/app/components/prodcuto-items/prodcuto-items.component.ts
+++ src/app/components/prodcuto-items/prodcuto-items.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-
+import { ProductosService } from "../../services/productos.service";
 
 @Component({
   selector: 'app-prodcuto-items',
@@ -8,11 +8,23 @@ import { ActivatedRoute } from "@angular/router";
   styles: []
 })
 export class ProdcutoItemsComponent {
+  producto:any = undefined;
+  cod:string =undefined;
 
-  constructor( private route:ActivatedRoute ) {
+  constructor( private route:ActivatedRoute,
+                private _ps: ProductosService ) {
     route.params.subscribe( parametros=>{
-      console.log( parametros );
-      console.log( parametros['id'] );  
+      //console.log( parametros );
+      //console.log( parametros['id'] );
+      _ps.cargar_producto( parametros['id'] )
+      .subscribe( res=> {
+
+        this.cod = parametros['id'];
+        //console.log( res.json());
+        this.producto = res.json(); //para que no tengamos que llamar la función 2 veces
+        console.log( this.producto ); //de lo contrario solo consume memoria 2 veces
+
+      });
     })
     //para obtener el parámetro
    }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,6 +11,10 @@ export class ProductosService {
     this.cargar_productos();
   }
 
+  public cargar_producto( cod:string ){
+    return this.http.get(`https://melisaproyecto.firebaseio.com/productos/${ cod }.json`);
+  }
+
   public cargar_productos(){
 
     this.cargando_p = true;
